b6c20ca1895e7633b32eb3ce727b384950801403
Tiansir-wg <15872397013@163.com>
2021-04-14 21:55:39 +0800

    子数组的最大累加和

--- src/edu/hit/NC19.java
+++ src/edu/hit/NC19.java
@@ -6,6 +6,7 @@ package edu.hit;
  * https://www.nowcoder.com/practice/554aa508dd5d4fefbf0f86e5fe953abd?tpId=117&tqId=37797&companyId=665&rp=1&ru=%2Fcompany%2Fhome%2Fcode%2F665&qru=%2Fta%2Fjob-code-high%2Fquestion-ranking&tab=answerKey
  */
 public class NC19 {
+
     public int maxsumofSubarray (int[] arr) {
         // dp[i]表示以第i个元素结尾的子数组的最大和
         int[] dp = new int[arr.length];
@@ -26,7 +27,7 @@ public class NC19 {
 
     public static void main(String[] args) {
         NC19 nc19 = new NC19();
-        int[] arr = {1, -2, 3, 5, -2, 6, -1};
+        int[] arr = {1, -2, 3, 5, -2, 6, -1, 2};
         System.out.println(nc19.maxsumofSubarray(arr));
     }
 }
